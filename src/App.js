6b850387765b664ff21b0660e65fe9242d842097
pokorowski <pawelokorowski@gmail.com>
2020-04-02 16:59:32 +0200

    branch test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello
+          It is such an awesome feature
         </p>
         <a
           className="App-link"
